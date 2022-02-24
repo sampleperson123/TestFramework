0a5578adf4df7ceb14bdd521cb01914234233ac8
Kapilesh Rajput <rajputkapilesh@outlook.com>
2022-02-24 12:25:52 +0530

    Added framework

---
+++ TestFramework/TestFramework/TestFramework.swift
@@ -0,0 +1,15 @@
+//
+//  TestFramework.swift
+//  TestFramework
+//
+//  Created by Kapilesh Rajput on 24/02/22.
+//
+
+import Foundation
+
+public struct TestFramework {
+    
+    public static func helloWorld(){
+        print("Hello World...")
+    }
+}
